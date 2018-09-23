4e9ad56ee073a96a4181a66eda6a9c2ddf23daba
Elliot <elliotdines1@gmail.com>
2018-09-23 13:03:35 +0100

    start of app

---
+++ Reciept Scanner/Reciept Scanner/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  Reciept Scanner
+//
+//  Created by Elliot Dines on 08/09/2018.
+//  Copyright © 2018 Elliot Dines. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBAction func captureImage(_ sender: UIButton) {
+        
+        print("This is the test running! ")
+        
+        
+    }
+
+}
+
